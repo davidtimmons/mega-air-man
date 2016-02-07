a87d584e7de1f1855d04922ee5e9fc101fc2d853
David Timmons <github@timmons.io>
2016-02-06 19:01:27 -0600

    Added additional notes.

--- src/elm/Main.elm
+++ src/elm/Main.elm
@@ -10,21 +10,24 @@ import Html exposing (Html)
 import StartApp
 import Task exposing (Task)
 
-{-|
+{-| This module orchestrates the application. It initializes state, defines
+input signals to feed into the app, and opens ports that work with Tasks.
+
+# Elm Architecture
+@docs app, main, tasks
 -}
 
 
-{-| This function uses a Config record to return an App record composed of
-three different signals. <main> uses the <html> signal. <model> is not often
-needed according to the official <StartApp#2.0.2> documentation. The <tasks>
-port uses the <tasks> value.
+{-| <app> uses a Config record to return an App record composed of
+three different signals: <main>, <model>, <tasks>. <model> is not often
+needed according to the official <StartApp#2.0.2> documentation.
 
     -- <Task Never ()> is a task that never fails and returns nothing when
     -- it succeeds (<()> is an empty I/O action result in Haskell.)
     type alias App model =
-      { html : Signal Html
-      , model : Signal model
-      , tasks : Signal (Task Never ())
+      { html : Signal Html -- Used in <main>.
+      , model : Signal model -- Not used.
+      , tasks : Signal (Task Never ()) -- Used in <tasks>.
       }
 -}
 app : StartApp.App Arena.Model
@@ -33,30 +36,23 @@ app =
     { init = Arena.init
     , update = Arena.update
     , view = Arena.view
+    -- <inputs> captures both vanilla Signals and external port data.
     , inputs = []
     }
 
 
-{-|
+{-| <main> is the application entry point. It captures the current visual
+representation of the app.
 -}
 main : Signal Html
 main =
   app.html
 
 
-{-| Tasks run in response to events. They are similar to signals but work with
-code and programs external to Elm by sending and receiving data through a port.
+{-| <tasks> opens a port that runs Task(s) in response to events. They are
+similar to signals but work with code external to Elm by sending and receiving
+data through a port (similar to FFI in Haskell).
 -}
 port tasks : Signal (Task Effects.Never ())
 port tasks =
   app.tasks
-
-{--
-main : Signal Html
-main =
-  start
-    { model = AirMan.init 0 0 0 0 AirMan.Right
-    , update = AirMan.update
-    , view = AirMan.view
-    }
---}
